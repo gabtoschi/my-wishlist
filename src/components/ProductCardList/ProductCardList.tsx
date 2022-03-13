8403196df99ef41aaee724653899ecb12eb3e2ae
Gabriel Toschi <gabriel.toschi@hotmail.com>
2022-03-13 15:31:49 -0300

    Fix missing key property on product card list

--- src/components/ProductCardList/ProductCardList.tsx
+++ src/components/ProductCardList/ProductCardList.tsx
@@ -6,7 +6,7 @@ const ProductCardList = ({ products }: ProductCardListProps) => {
     <div className="row">
       {products.map((product) => (
         <div
-          id={product.id.toString()}
+          key={product.id.toString()}
           className="col-6 col-md-4 col-lg-3 pb-4"
         >
           <ProductCard {...product} />
