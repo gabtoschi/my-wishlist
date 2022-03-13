da46302b16d93671012d3389619fc5f3413ee8d2
Gabriel Toschi <gabriel.toschi@hotmail.com>
2022-03-13 12:15:47 -0300

    Creates a blank App

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders hello world", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const helloWorldElement = screen.getByText(/hello, world/i);
+  expect(helloWorldElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,7 @@
 import "./App.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="container">Hello, world!</div>;
 }
 
 export default App;
